f3606ce605625d19561fe05f928ec88d4eec5ac7
AlbertSerica <943914044@qq.com>
2017-09-18 22:13:09 +0800

    delete unnecessary files

--- render.js
+++
@@ -1,6 +0,0 @@
-var $ = require("./app/js/jquery-3.2.1.min.js");
-var jQuery = require("./app/js/jquery-3.2.1.min.js");
-var $ = jQuery.noConflict();
-require("./app/js/materialize.min.js");
-require("./app/js/draw.js");
-require("./app.js");
--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    entry: "./render.js",//已多次提及的唯一入口文件
-    output: {
-      filename: "./render.bundle.js"//打包后输出文件的文件名
-    }
-  }
-  
